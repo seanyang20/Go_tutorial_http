adf419188a7a6cbccfa03b05f1aabfdbe63d8aa1
Sean <sean.yang@econommi.io>
2023-05-02 21:06:02 -0700

    read and write interface

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -13,5 +14,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// working with the Read function here
+	// this is saying to give me a byte slice that can contain 99999 elements
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// -------
+	// condensed version of the above
+	// --------
+	io.Copy(os.Stdout, resp.Body)
+
 }
